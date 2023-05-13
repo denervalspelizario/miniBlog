44f66d0b0047b5e416cf3d7972ecf891a663bf72
denervalspelizario <denervalspelizario@gmail.com>
2023-05-12 22:32:12 -0300

    update miniBlog adicaod e navbar e footer ja com estilizacao

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import './App.css';
 // Pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <div className="container">
           <Routes>
             <Route  path="/" element={<Home />} />
             <Route  path="about" element={<About />} />
           </Routes>
         </div>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Navbar.module.css';
+import { NavLink } from 'react-router-dom'; 
+
+export default function Navbar(){
+  return (
+    <nav className={styles.navbar}>
+      <NavLink className={styles.brand}>
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink 
+            to='/' 
+            className={({isActive}) => (isActive ? styles.active : "") }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/about'
+            className={({isActive}) => (isActive ? styles.active : "") } 
+          >
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
